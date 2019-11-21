612bad0d0816a968091633b627e2c5b183bd2ae2
NataliaLopatkina <51717101+NataliaLopatkina@users.noreply.github.com>
2019-11-21 18:05:06 +0300

    Change auth

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,14 +6,17 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AuthModule } from './modules/auth/auth.module';
+
 import { MainComponent } from './components/main/main.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule,
